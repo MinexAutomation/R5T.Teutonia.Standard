a5a83ae61bec04d945051803c8c2df3cefce82e3
MinexAutomation <minex.automation@gmail.com>
2020-02-18 22:48:05 -0800

    Added IServiceCollection extensions.

--- source/R5T.Teutonia.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Teutonia.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Teutonia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy.Standard;
+
+using R5T.Teutonia.Default;
+
+
+namespace R5T.Teutonia.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IFileSystemCloningOperator"/> service.
+        /// </summary>
+        public static IServiceCollection AddFileSystemCloningOperator(this IServiceCollection services)
+        {
+            services.AddDefaultFileSystemCloningOperator(
+                services.AddDefaultFileSystemCloningDifferencerAction(),
+                services.AddStringlyTypedPathOperatorAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IFileSystemCloningOperator"/> service.
+        /// </summary>
+        public static ServiceAction<IFileSystemCloningOperator> AddFileSystemCloningOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IFileSystemCloningOperator>(() => services.AddFileSystemCloningOperator());
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IFileSystemCloningDifferencer"/> service.
+        /// </summary>
+        public static IServiceCollection AddFileSystemCloningDifferencer(this IServiceCollection services)
+        {
+            services.AddDefaultFileSystemCloningDifferencer();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IFileSystemCloningDifferencer"/> service.
+        /// </summary>
+        public static ServiceAction<IFileSystemCloningDifferencer> AddFileSystemCloningDifferencerAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IFileSystemCloningDifferencer>(() => services.AddFileSystemCloningDifferencer());
+            return serviceAction;
+        }
+    }
+}
